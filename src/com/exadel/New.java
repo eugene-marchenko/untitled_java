40aab6788be41998d632176afebdf8794db8f452
Eugene Marchenko <3.marchenko@gmail.com>
2015-02-25 02:54:34 +0300

    Initial commit

---
+++ src/com/exadel/New.java
@@ -0,0 +1,10 @@
+package com.exadel;
+
+/**
+ * Created by ymarchanka on 2/25/15.
+ */
+public class New {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
